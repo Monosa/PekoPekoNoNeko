07fd639ab14ed13ae12f5eaf7867fa93d5aec583
vic <mariavba@ucm.es>
2020-02-03 22:44:20 +0100

    Primer intento en funcionalidad de elección de canción (fallido)

--- TFG/Canciones/Canciones.js
+++ TFG/Canciones/Canciones.js
@@ -4,15 +4,17 @@ const path = require("path");
 const multer = require("multer");
 const DAOCanciones = require("./DAOCanciones.js");
 const config = require("../config");
+const main = require("../public/js/main.js");
+const bodyParser = require("body-parser");
 
 const Canciones = express.Router();
 
 const pool = mysql.createPool(config.mysqlConfig);
-
 const daoCanciones = new DAOCanciones(pool);
 
-Canciones.get("/", function (request, response) {
+Canciones.use(bodyParser.json());
 
+Canciones.get("/", function (request, response) {
     daoCanciones.getListaCanciones(function (error, listaCanciones) {
         //Faltan las comprobaciones
         if (error) {
@@ -23,6 +25,18 @@ Canciones.get("/", function (request, response) {
             response.render("songSelection", { canciones: listaCanciones, errorMsg: null });
         }
     })
+});
+
+Canciones.get("/play", function(request, response){
+    daoCanciones.getCancion(function(error, cancion){
+        if(error){
+            response.status(500);
+            response.render("songSelection", { canciones: null, errorMsg: `${error.message}`});
+        }else{
+            response.status(200);
+            response.json(cancion);
+        }
+    });
 });
 
 module.exports = Canciones;
--- TFG/public/js/main.js
+++ TFG/public/js/main.js
@@ -1,6 +1,6 @@
 // La letra j es la tecla 106 y la k es la 107
 // La letra s tiene el código 115 y la d el 100
-var tiempos = [{tiempo : 239, tecla : 106, tecla2 : 100, tipo : 50},{tiempo : 2820, tecla : 106, tecla2 : 115, tipo : 50},{tiempo : 5567, tecla : 106, tecla2 : 115, tipo : 25},{tiempo : 6064, tecla : 106, tecla2 : 100, tipo : 25},{tiempo : 9634, tecla : 106, tecla2 : 115, tipo : 50},{tiempo : 10359, tecla : 107, tecla2 : 100, tipo : 25},{tiempo : 13864, tecla : 106, tecla2 : 100, tipo : 25},{tiempo : 14767, tecla : 106, tecla2 : 115, tipo : 50},{tiempo : 17053, tecla : 107, tecla2 : 100, tipo : 50},{tiempo : 19239, tecla : 106, tecla2 : 100, tipo : 50},{tiempo : 21054, tecla : 107, tecla2 : 115, tipo : 50},{tiempo : 23362, tecla : 107, tecla2 : 100, tipo : 25},{tiempo : 24258, tecla : 107, tecla2 : 100, tipo : 25},{tiempo : 26939, tecla : 106, tecla2 : 115, tipo : 50},{tiempo : 28574, tecla : 106, tecla2 : 100, tipo : 50},{tiempo : 30563, tecla : 107, tecla2 : 100, tipo : 50},{tiempo : 32058, tecla : 107, tecla2 : 115, tipo : 25},{tiempo : 35653, tecla : 106, tecla2 : 115, tipo : 25},{tiempo : 36746, tecla : 106, tecla2 : 100, tipo : 50},{tiempo : 38632, tecla : 107, tecla2 : 115, tipo : 50},{tiempo : 40080, tecla : 106, tecla2 : 115, tipo : 25},{tiempo : 43988, tecla : 107, tecla2 : 100, tipo : 50},{tiempo : 45058, tecla : 106, tecla2 : 115, tipo : 50},{tiempo : 47421, tecla : 107, tecla2 : 100, tipo : 50},{tiempo : 49391, tecla : 106, tecla2 : 100, tipo : 50},{tiempo : 51352, tecla : 106, tecla2 : 100, tipo : 50},{tiempo : 52788, tecla : 106, tecla2 : 115, tipo : 25},{tiempo : 54971, tecla : 106, tecla2 : 100, tipo : 50},{tiempo : 57787, tecla : 107, tecla2 : 100, tipo : 50},{tiempo : 58687, tecla : 106, tecla2 : 100, tipo : 50},{tiempo : 61893, tecla : 106, tecla2 : 115, tipo : 25},{tiempo : 62255, tecla : 106, tecla2 : 115, tipo : 50},{tiempo : 64470, tecla : 107, tecla2 : 115, tipo : 50},{tiempo : 66076, tecla : 107, tecla2 : 100, tipo : 25},{tiempo : 69041, tecla : 106, tecla2 : 100, tipo : 50},{tiempo : 70318, tecla : 107, tecla2 : 115, tipo : 25},{tiempo : 73411, tecla : 107, tecla2 : 115, tipo : 50},{tiempo : 75302, tecla : 107, tecla2 : 115, tipo : 50},{tiempo : 77449, tecla : 107, tecla2 : 115, tipo : 25},{tiempo : 78780, tecla : 106, tecla2 : 115, tipo : 50},{tiempo : 80498, tecla : 107, tecla2 : 115, tipo : 25},{tiempo : 82379, tecla : 106, tecla2 : 100, tipo : 25},{tiempo : 84085, tecla : 106, tecla2 : 115, tipo : 50},{tiempo : 87318, tecla : 107, tecla2 : 100, tipo : 25},{tiempo : 89890, tecla : 106, tecla2 : 115, tipo : 50},{tiempo : 90749, tecla : 106, tecla2 : 100, tipo : 25},{tiempo : 93245, tecla : 106, tecla2 : 100, tipo : 25},{tiempo : 94954, tecla : 107, tecla2 : 115, tipo : 50},{tiempo : 96649, tecla : 106, tecla2 : 100, tipo : 25},{tiempo : 98863, tecla : 107, tecla2 : 100, tipo : 50},{tiempo : 101351, tecla : 107, tecla2 : 115, tipo : 25},{tiempo : 103997, tecla : 107, tecla2 : 115, tipo : 25},{tiempo : 104101, tecla : 107, tecla2 : 100, tipo : 25},{tiempo : 106530, tecla : 107, tecla2 : 100, tipo : 25},{tiempo : 108718, tecla : 106, tecla2 : 115, tipo : 25},{tiempo : 110627, tecla : 107, tecla2 : 115, tipo : 50},{tiempo : 113313, tecla : 106, tecla2 : 100, tipo : 50},{tiempo : 114187, tecla : 107, tecla2 : 115, tipo : 25},{tiempo : 116161, tecla : 107, tecla2 : 100, tipo : 50},{tiempo : 118496, tecla : 107, tecla2 : 100, tipo : 25},{tiempo : 121506, tecla : 106, tecla2 : 115, tipo : 25},{tiempo : 122744, tecla : 106, tecla2 : 115, tipo : 50},{tiempo : 124561, tecla : 106, tecla2 : 100, tipo : 25},{tiempo : 126337, tecla : 106, tecla2 : 100, tipo : 25},{tiempo : 128504, tecla : 106, tecla2 : 115, tipo : 50},{tiempo : 131635, tecla : 106, tecla2 : 100, tipo : 25},{tiempo : 132801, tecla : 107, tecla2 : 115, tipo : 25},{tiempo : 134735, tecla : 107, tecla2 : 100, tipo : 50},{tiempo : 136487, tecla : 106, tecla2 : 100, tipo : 50},{tiempo : 138985, tecla : 107, tecla2 : 115, tipo : 25},{tiempo : 140104, tecla : 107, tecla2 : 100, tipo : 50},{tiempo : 142820, tecla : 107, tecla2 : 115, tipo : 50},{tiempo : 144874, tecla : 106, tecla2 : 115, tipo : 25},{tiempo : 146377, tecla : 106, tecla2 : 115, tipo : 50},{tiempo : 149598, tecla : 106, tecla2 : 115, tipo : 25},{tiempo : 150705, tecla : 107, tecla2 : 115, tipo : 50},{tiempo : 153589, tecla : 106, tecla2 : 115, tipo : 25},{tiempo : 154424, tecla : 106, tecla2 : 115, tipo : 25},{tiempo : 157302, tecla : 106, tecla2 : 100, tipo : 25},{tiempo : 158215, tecla : 107, tecla2 : 100, tipo : 25},{tiempo : 160542, tecla : 106, tecla2 : 100, tipo : 25},{tiempo : 162456, tecla : 107, tecla2 : 100, tipo : 50},{tiempo : 165286, tecla : 106, tecla2 : 100, tipo : 25},{tiempo : 167001, tecla : 106, tecla2 : 115, tipo : 50},{tiempo : 168013, tecla : 106, tecla2 : 100, tipo : 50},{tiempo : 171625, tecla : 107, tecla2 : 115, tipo : 25},{tiempo : 173646, tecla : 106, tecla2 : 115, tipo : 25},{tiempo : 174754, tecla : 107, tecla2 : 100, tipo : 25},{tiempo : 176048, tecla : 107, tecla2 : 100, tipo : 50},{tiempo : 179871, tecla : 107, tecla2 : 100, tipo : 50},{tiempo : 180200, tecla : 107, tecla2 : 115, tipo : 25},{tiempo : 183893, tecla : 107, tecla2 : 100, tipo : 25},{tiempo : 184200, tecla : 107, tecla2 : 100, tipo : 25},{tiempo : 187933, tecla : 107, tecla2 : 115, tipo : 50},{tiempo : 189256, tecla : 106, tecla2 : 100, tipo : 25},{tiempo : 190053, tecla : 106, tecla2 : 115, tipo : 50},{tiempo : 193169, tecla : 107, tecla2 : 115, tipo : 25},{tiempo : 194366, tecla : 106, tecla2 : 100, tipo : 50},{tiempo : 196555, tecla : 107, tecla2 : 100, tipo : 50},{tiempo : 199554, tecla : 106, tecla2 : 100, tipo : 50}];
+//var tiempos = [{tiempo : 239, tecla : 106, tecla2 : 100, tipo : 50},{tiempo : 2820, tecla : 106, tecla2 : 115, tipo : 50},{tiempo : 5567, tecla : 106, tecla2 : 115, tipo : 25},{tiempo : 6064, tecla : 106, tecla2 : 100, tipo : 25},{tiempo : 9634, tecla : 106, tecla2 : 115, tipo : 50},{tiempo : 10359, tecla : 107, tecla2 : 100, tipo : 25},{tiempo : 13864, tecla : 106, tecla2 : 100, tipo : 25},{tiempo : 14767, tecla : 106, tecla2 : 115, tipo : 50},{tiempo : 17053, tecla : 107, tecla2 : 100, tipo : 50},{tiempo : 19239, tecla : 106, tecla2 : 100, tipo : 50},{tiempo : 21054, tecla : 107, tecla2 : 115, tipo : 50},{tiempo : 23362, tecla : 107, tecla2 : 100, tipo : 25},{tiempo : 24258, tecla : 107, tecla2 : 100, tipo : 25},{tiempo : 26939, tecla : 106, tecla2 : 115, tipo : 50},{tiempo : 28574, tecla : 106, tecla2 : 100, tipo : 50},{tiempo : 30563, tecla : 107, tecla2 : 100, tipo : 50},{tiempo : 32058, tecla : 107, tecla2 : 115, tipo : 25},{tiempo : 35653, tecla : 106, tecla2 : 115, tipo : 25},{tiempo : 36746, tecla : 106, tecla2 : 100, tipo : 50},{tiempo : 38632, tecla : 107, tecla2 : 115, tipo : 50},{tiempo : 40080, tecla : 106, tecla2 : 115, tipo : 25},{tiempo : 43988, tecla : 107, tecla2 : 100, tipo : 50},{tiempo : 45058, tecla : 106, tecla2 : 115, tipo : 50},{tiempo : 47421, tecla : 107, tecla2 : 100, tipo : 50},{tiempo : 49391, tecla : 106, tecla2 : 100, tipo : 50},{tiempo : 51352, tecla : 106, tecla2 : 100, tipo : 50},{tiempo : 52788, tecla : 106, tecla2 : 115, tipo : 25},{tiempo : 54971, tecla : 106, tecla2 : 100, tipo : 50},{tiempo : 57787, tecla : 107, tecla2 : 100, tipo : 50},{tiempo : 58687, tecla : 106, tecla2 : 100, tipo : 50},{tiempo : 61893, tecla : 106, tecla2 : 115, tipo : 25},{tiempo : 62255, tecla : 106, tecla2 : 115, tipo : 50},{tiempo : 64470, tecla : 107, tecla2 : 115, tipo : 50},{tiempo : 66076, tecla : 107, tecla2 : 100, tipo : 25},{tiempo : 69041, tecla : 106, tecla2 : 100, tipo : 50},{tiempo : 70318, tecla : 107, tecla2 : 115, tipo : 25},{tiempo : 73411, tecla : 107, tecla2 : 115, tipo : 50},{tiempo : 75302, tecla : 107, tecla2 : 115, tipo : 50},{tiempo : 77449, tecla : 107, tecla2 : 115, tipo : 25},{tiempo : 78780, tecla : 106, tecla2 : 115, tipo : 50},{tiempo : 80498, tecla : 107, tecla2 : 115, tipo : 25},{tiempo : 82379, tecla : 106, tecla2 : 100, tipo : 25},{tiempo : 84085, tecla : 106, tecla2 : 115, tipo : 50},{tiempo : 87318, tecla : 107, tecla2 : 100, tipo : 25},{tiempo : 89890, tecla : 106, tecla2 : 115, tipo : 50},{tiempo : 90749, tecla : 106, tecla2 : 100, tipo : 25},{tiempo : 93245, tecla : 106, tecla2 : 100, tipo : 25},{tiempo : 94954, tecla : 107, tecla2 : 115, tipo : 50},{tiempo : 96649, tecla : 106, tecla2 : 100, tipo : 25},{tiempo : 98863, tecla : 107, tecla2 : 100, tipo : 50},{tiempo : 101351, tecla : 107, tecla2 : 115, tipo : 25},{tiempo : 103997, tecla : 107, tecla2 : 115, tipo : 25},{tiempo : 104101, tecla : 107, tecla2 : 100, tipo : 25},{tiempo : 106530, tecla : 107, tecla2 : 100, tipo : 25},{tiempo : 108718, tecla : 106, tecla2 : 115, tipo : 25},{tiempo : 110627, tecla : 107, tecla2 : 115, tipo : 50},{tiempo : 113313, tecla : 106, tecla2 : 100, tipo : 50},{tiempo : 114187, tecla : 107, tecla2 : 115, tipo : 25},{tiempo : 116161, tecla : 107, tecla2 : 100, tipo : 50},{tiempo : 118496, tecla : 107, tecla2 : 100, tipo : 25},{tiempo : 121506, tecla : 106, tecla2 : 115, tipo : 25},{tiempo : 122744, tecla : 106, tecla2 : 115, tipo : 50},{tiempo : 124561, tecla : 106, tecla2 : 100, tipo : 25},{tiempo : 126337, tecla : 106, tecla2 : 100, tipo : 25},{tiempo : 128504, tecla : 106, tecla2 : 115, tipo : 50},{tiempo : 131635, tecla : 106, tecla2 : 100, tipo : 25},{tiempo : 132801, tecla : 107, tecla2 : 115, tipo : 25},{tiempo : 134735, tecla : 107, tecla2 : 100, tipo : 50},{tiempo : 136487, tecla : 106, tecla2 : 100, tipo : 50},{tiempo : 138985, tecla : 107, tecla2 : 115, tipo : 25},{tiempo : 140104, tecla : 107, tecla2 : 100, tipo : 50},{tiempo : 142820, tecla : 107, tecla2 : 115, tipo : 50},{tiempo : 144874, tecla : 106, tecla2 : 115, tipo : 25},{tiempo : 146377, tecla : 106, tecla2 : 115, tipo : 50},{tiempo : 149598, tecla : 106, tecla2 : 115, tipo : 25},{tiempo : 150705, tecla : 107, tecla2 : 115, tipo : 50},{tiempo : 153589, tecla : 106, tecla2 : 115, tipo : 25},{tiempo : 154424, tecla : 106, tecla2 : 115, tipo : 25},{tiempo : 157302, tecla : 106, tecla2 : 100, tipo : 25},{tiempo : 158215, tecla : 107, tecla2 : 100, tipo : 25},{tiempo : 160542, tecla : 106, tecla2 : 100, tipo : 25},{tiempo : 162456, tecla : 107, tecla2 : 100, tipo : 50},{tiempo : 165286, tecla : 106, tecla2 : 100, tipo : 25},{tiempo : 167001, tecla : 106, tecla2 : 115, tipo : 50},{tiempo : 168013, tecla : 106, tecla2 : 100, tipo : 50},{tiempo : 171625, tecla : 107, tecla2 : 115, tipo : 25},{tiempo : 173646, tecla : 106, tecla2 : 115, tipo : 25},{tiempo : 174754, tecla : 107, tecla2 : 100, tipo : 25},{tiempo : 176048, tecla : 107, tecla2 : 100, tipo : 50},{tiempo : 179871, tecla : 107, tecla2 : 100, tipo : 50},{tiempo : 180200, tecla : 107, tecla2 : 115, tipo : 25},{tiempo : 183893, tecla : 107, tecla2 : 100, tipo : 25},{tiempo : 184200, tecla : 107, tecla2 : 100, tipo : 25},{tiempo : 187933, tecla : 107, tecla2 : 115, tipo : 50},{tiempo : 189256, tecla : 106, tecla2 : 100, tipo : 25},{tiempo : 190053, tecla : 106, tecla2 : 115, tipo : 50},{tiempo : 193169, tecla : 107, tecla2 : 115, tipo : 25},{tiempo : 194366, tecla : 106, tecla2 : 100, tipo : 50},{tiempo : 196555, tecla : 107, tecla2 : 100, tipo : 50},{tiempo : 199554, tecla : 106, tecla2 : 100, tipo : 50}];
 var particles = [];
 var puntos = 0, puntos2 = 0;
 var startTime;
@@ -12,20 +12,35 @@ var contadorBien = 0;
 var contadorMal = 0;
 var canvas;
 
+// $(function () {
+//   document.onkeypress = this.mostrarInformacionTecla;
+//   document.onkeyup = this.comprobarPuntos;
+  
+//   iniciarPuntos();
+//   drawInitialCanvas();
+
+//   settings = {
+//     density: 1,
+//     particleSize: 25,
+//     startingX: canvas.width + 25,
+//     startingY: 150
+//   };
+
+// });
 
 window.onload = function () {
-  document.onkeypress = this.mostrarInformacionTecla;
-  document.onkeyup = this.comprobarPuntos;
+   document.onkeypress = this.mostrarInformacionTecla;
+   document.onkeyup = this.comprobarPuntos;
 
-  iniciarPuntos();
-  drawInitialCanvas();
+   iniciarPuntos();
+   drawInitialCanvas();
 
-  settings = {
-    density: 1,
-    particleSize: 25,
-    startingX: canvas.width + 25,
-    startingY: 150
-  };
+   settings = {
+     density: 1,
+     particleSize: 25,
+     startingX: canvas.width + 25,
+     startingY: 150
+   };
 };
 
 //  Places a background image in a background canvas and draws the square over it
@@ -82,114 +97,136 @@ function drawPattern(context, canvas, bgImg) {
 }
 
 
-$("#player").bind("ended", function () {
-  var x = document.getElementById("myAudio");
-  x.play();
-  comprobar();
+function cargarJuego(){
+  $.ajax({
+    method: "GET",
+    url: "/canciones/play",
 
-  var canvas2, context2;
-  var canvas = document.getElementById("canvas-1");
-  var context = canvas.getContext("2d");
-
-  if (multiplayer) {
-    canvas2 = document.getElementById("canvas-2");
-    context2 = canvas2.getContext("2d");
-  }
-
-  for (var i = 0; i < tiempos.length; i++) {
-    particles.push({
-      x: settings.startingX,
-      y: settings.startingY,
-      timing: tiempos[i].tiempo,
-      size: tiempos[i].tipo,
-      vx: 20,
-      moving: true,
-      tecla: tiempos[i].tecla,
-	  tecla2: tiempos[i].tecla2,
-    });
-  }
-  playing = particles[0];
-  
-
-  window.requestAnimationFrame(animate);
-
-  function animate(time) {
-    // set startTime if it isn't already set
-    if (!startTime) {
-      startTime = time;
-    }
-    // calc elapsedTime
-    var elapsedTime = time - startTime;
-
-    context.clearRect(0, 0, canvas.width, canvas.height);
-
-    if (multiplayer)
-      context2.clearRect(0, 0, canvas2.width, canvas2.height);
-
-    // assume no further animating is necessary
-    // The for-loop may change the assumption 
-    var continueAnimating = false;
-    for (var i = 0; i < particles.length; i++) {
-      var part = particles[i];
-
-      // update this circle & report if it wasMoved
-      var wasMoved = update(part, elapsedTime);
-      // if it wasMoved, then change assumption to continueAnimating
-      if (wasMoved || part.moving) {
-        continueAnimating = true;
-      }
-      // draw this arc at its current position
-      drawParticle(part);
-    }
-
-    // if update() reported that it moved something
-    // then request another animation loop
-    if (continueAnimating)
-      window.requestAnimationFrame(animate);
-  }
-
-  function update(part, elapsedTime) {
-    // has this arc's animation delay been reached by elapsedTime
-    //console.log("tiempo de particula: " + part.timing);
-    //console.log("Elapsed time: " + elapsedTime);
-    if (elapsedTime >= part.timing) {
-      // is this arc still visible on the canvas
-      if (part.x > -part.size) {
-        // if yes+yes, move this arc by the specified moveX
-        part.x -= part.vx;
-        if (part.x <= 0) {
-          part.moving = false;
+    success: function (data, textStatus, jqXHR) {
+      var tiempos = data.value;
+      $("#player").bind("ended", function () {
+        var x = document.getElementById("myAudio");
+        x.play();
+        comprobar();
+      
+        var canvas2, context2;
+        var canvas = document.getElementById("canvas-1");
+        var context = canvas.getContext("2d");
+      
+        if (multiplayer) {
+          canvas2 = document.getElementById("canvas-2");
+          context2 = canvas2.getContext("2d");
         }
-        // report that we moved this arc
-        return (true);
-      }
+      
+        for (var i = 0; i < tiempos.length; i++) {
+          particles.push({
+            x: settings.startingX,
+            y: settings.startingY,
+            timing: tiempos[i].tiempo,
+            size: tiempos[i].tipo,
+            vx: 20,
+            moving: true,
+            tecla: tiempos[i].tecla,
+          tecla2: tiempos[i].tecla2,
+          });
+        }
+        playing = particles[0];
+        
+      
+        window.requestAnimationFrame(animate);
+      
+        function animate(time) {
+          // set startTime if it isn't already set
+          if (!startTime) {
+            startTime = time;
+          }
+          // calc elapsedTime
+          var elapsedTime = time - startTime;
+      
+          context.clearRect(0, 0, canvas.width, canvas.height);
+      
+          if (multiplayer)
+            context2.clearRect(0, 0, canvas2.width, canvas2.height);
+      
+          // assume no further animating is necessary
+          // The for-loop may change the assumption 
+          var continueAnimating = false;
+          for (var i = 0; i < particles.length; i++) {
+            var part = particles[i];
+      
+            // update this circle & report if it wasMoved
+            var wasMoved = update(part, elapsedTime);
+            // if it wasMoved, then change assumption to continueAnimating
+            if (wasMoved || part.moving) {
+              continueAnimating = true;
+            }
+            // draw this arc at its current position
+            drawParticle(part);
+          }
+      
+          // if update() reported that it moved something
+          // then request another animation loop
+          if (continueAnimating)
+            window.requestAnimationFrame(animate);
+        }
+      
+        function update(part, elapsedTime) {
+          // has this arc's animation delay been reached by elapsedTime
+          //console.log("tiempo de particula: " + part.timing);
+          //console.log("Elapsed time: " + elapsedTime);
+          if (elapsedTime >= part.timing) {
+            // is this arc still visible on the canvas
+            if (part.x > -part.size) {
+              // if yes+yes, move this arc by the specified moveX
+              part.x -= part.vx;
+              if (part.x <= 0) {
+                part.moving = false;
+              }
+              // report that we moved this arc
+              return (true);
+            }
+          }
+          //console.log("fin bucle con " + elapsedTime + " - " + part.timing);
+          // report that we didn't move this arc
+          return (false);
+        }
+      
+        function drawParticle(part) {
+          context.beginPath();
+          if (part.tecla === 107)
+            context.fillStyle = "cyan";
+          else if (part.tecla === 106)
+            context.fillStyle = "red";
+          context.arc(part.x, part.y, part.size, 0, Math.PI * 2);
+          context.fill();
+      
+          if (multiplayer) {
+            context2.beginPath();
+            if (part.tecla2 === 107)
+              context2.fillStyle = "cyan";
+            else if (part.tecla2 === 106)
+              context2.fillStyle = "red";
+            context2.arc(part.x, part.y, part.size, 0, Math.PI * 2);
+            context2.fill();
+          }
+        }
+      
+      });
+    },
+
+    error: function (jqXHR, testStatus, errorThrown) {
+        alert("Se ha producido un error: " + errorThrown);
     }
-    //console.log("fin bucle con " + elapsedTime + " - " + part.timing);
-    // report that we didn't move this arc
-    return (false);
-  }
+  })
+}
 
-  function drawParticle(part) {
-    context.beginPath();
-    if (part.tecla === 107)
-      context.fillStyle = "cyan";
-    else if (part.tecla === 106)
-      context.fillStyle = "red";
-    context.arc(part.x, part.y, part.size, 0, Math.PI * 2);
-    context.fill();
 
-    if (multiplayer) {
-      context2.beginPath();
-      if (part.tecla2 === 107)
-        context2.fillStyle = "cyan";
-      else if (part.tecla2 === 106)
-        context2.fillStyle = "red";
-      context2.arc(part.x, part.y, part.size, 0, Math.PI * 2);
-      context2.fill();
-    }
-  }
 
-});
+
+
+
+
+
 
 
 function mostrarInformacionTecla(evObject) {
@@ -279,3 +316,5 @@ function comprueba() {
     } else playing = -1;
   }
 }
+
+module.exports = main;
