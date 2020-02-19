b383b1a958b1390e70fd23a38785634bf590362b
lelex22 <alpedros@ucm.es>
2020-02-19 13:13:45 +0100

    Revert "Revert "Añadido docs y fondo""
    This reverts commit 041a23d0f485d8d6a15455e6d400e38996f75477.

---
+++ docs/Canciones/Canciones.js
@@ -0,0 +1,49 @@
+const express = require("express");
+//const mysql = require("mysql");
+const path = require("path");
+const multer = require("multer");
+const DAOCanciones = require("./DAOCanciones.js");
+const config = require("../config");
+//const main = require("../public/js/main.js");
+const bodyParser = require("body-parser");
+//var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/PekoPekoNoNeko";
+const Canciones = express.Router();
+
+//const pool = mysql.createPool(config.mysqlConfig);
+const daoCanciones = new DAOCanciones();
+
+Canciones.use(bodyParser.json());
+
+Canciones.get("/", function (request, response) {
+    daoCanciones.getListaCanciones(config.MongoClient, url, function (error, listaCanciones) {
+        //Faltan las comprobaciones
+        if (error) {
+            response.status(500);
+            console.log(`${error.message}`);
+        } else {
+            response.status(200);
+            response.render("songSelection", { canciones: listaCanciones, errorMsg: null });
+        }
+    })
+});
+
+Canciones.get("/play", function(request, response){
+    let idcancion = request.query.idcancion;
+    let iddificultad = request.query.iddificultad;
+
+    daoCanciones.getCancion(config.MongoClient, url, idcancion,iddificultad,function(error, cancion){
+
+        if(error){
+            response.status(500);
+            response.render("songSelection", { canciones: null, errorMsg: `${error.message}`});
+        }else{
+            // Incluir campos ocultos en el html, leer esos campos desde el .js
+            response.status(200);            
+            console.log(cancion[0]['Cancion']);
+            response.render("game", { tiempos: JSON.stringify(cancion[1]['value']['tiempos']), song: cancion[0]['Cancion'], errorMsg: null });
+        }
+    });
+});
+
+module.exports = Canciones;
---
+++ docs/public/js/main.js
@@ -0,0 +1,400 @@
+// La letra j es la tecla 106 y la k es la 107
+// La letra f tiene el código 102 y la d el 100
+//var tiempos = [{tiempo : 239, tecla : 106, tecla2 : 100, tipo : 50},{tiempo : 2820, tecla : 106, tecla2 : 115, tipo : 50},{tiempo : 5567, tecla : 106, tecla2 : 115, tipo : 25},{tiempo : 6064, tecla : 106, tecla2 : 100, tipo : 25},{tiempo : 9634, tecla : 106, tecla2 : 115, tipo : 50},{tiempo : 10359, tecla : 107, tecla2 : 100, tipo : 25},{tiempo : 13864, tecla : 106, tecla2 : 100, tipo : 25},{tiempo : 14767, tecla : 106, tecla2 : 115, tipo : 50},{tiempo : 17053, tecla : 107, tecla2 : 100, tipo : 50},{tiempo : 19239, tecla : 106, tecla2 : 100, tipo : 50},{tiempo : 21054, tecla : 107, tecla2 : 115, tipo : 50},{tiempo : 23362, tecla : 107, tecla2 : 100, tipo : 25},{tiempo : 24258, tecla : 107, tecla2 : 100, tipo : 25},{tiempo : 26939, tecla : 106, tecla2 : 115, tipo : 50},{tiempo : 28574, tecla : 106, tecla2 : 100, tipo : 50},{tiempo : 30563, tecla : 107, tecla2 : 100, tipo : 50},{tiempo : 32058, tecla : 107, tecla2 : 115, tipo : 25},{tiempo : 35653, tecla : 106, tecla2 : 115, tipo : 25},{tiempo : 36746, tecla : 106, tecla2 : 100, tipo : 50},{tiempo : 38632, tecla : 107, tecla2 : 115, tipo : 50},{tiempo : 40080, tecla : 106, tecla2 : 115, tipo : 25},{tiempo : 43988, tecla : 107, tecla2 : 100, tipo : 50},{tiempo : 45058, tecla : 106, tecla2 : 115, tipo : 50},{tiempo : 47421, tecla : 107, tecla2 : 100, tipo : 50},{tiempo : 49391, tecla : 106, tecla2 : 100, tipo : 50},{tiempo : 51352, tecla : 106, tecla2 : 100, tipo : 50},{tiempo : 52788, tecla : 106, tecla2 : 115, tipo : 25},{tiempo : 54971, tecla : 106, tecla2 : 100, tipo : 50},{tiempo : 57787, tecla : 107, tecla2 : 100, tipo : 50},{tiempo : 58687, tecla : 106, tecla2 : 100, tipo : 50},{tiempo : 61893, tecla : 106, tecla2 : 115, tipo : 25},{tiempo : 62255, tecla : 106, tecla2 : 115, tipo : 50},{tiempo : 64470, tecla : 107, tecla2 : 115, tipo : 50},{tiempo : 66076, tecla : 107, tecla2 : 100, tipo : 25},{tiempo : 69041, tecla : 106, tecla2 : 100, tipo : 50},{tiempo : 70318, tecla : 107, tecla2 : 115, tipo : 25},{tiempo : 73411, tecla : 107, tecla2 : 115, tipo : 50},{tiempo : 75302, tecla : 107, tecla2 : 115, tipo : 50},{tiempo : 77449, tecla : 107, tecla2 : 115, tipo : 25},{tiempo : 78780, tecla : 106, tecla2 : 115, tipo : 50},{tiempo : 80498, tecla : 107, tecla2 : 115, tipo : 25},{tiempo : 82379, tecla : 106, tecla2 : 100, tipo : 25},{tiempo : 84085, tecla : 106, tecla2 : 115, tipo : 50},{tiempo : 87318, tecla : 107, tecla2 : 100, tipo : 25},{tiempo : 89890, tecla : 106, tecla2 : 115, tipo : 50},{tiempo : 90749, tecla : 106, tecla2 : 100, tipo : 25},{tiempo : 93245, tecla : 106, tecla2 : 100, tipo : 25},{tiempo : 94954, tecla : 107, tecla2 : 115, tipo : 50},{tiempo : 96649, tecla : 106, tecla2 : 100, tipo : 25},{tiempo : 98863, tecla : 107, tecla2 : 100, tipo : 50},{tiempo : 101351, tecla : 107, tecla2 : 115, tipo : 25},{tiempo : 103997, tecla : 107, tecla2 : 115, tipo : 25},{tiempo : 104101, tecla : 107, tecla2 : 100, tipo : 25},{tiempo : 106530, tecla : 107, tecla2 : 100, tipo : 25},{tiempo : 108718, tecla : 106, tecla2 : 115, tipo : 25},{tiempo : 110627, tecla : 107, tecla2 : 115, tipo : 50},{tiempo : 113313, tecla : 106, tecla2 : 100, tipo : 50},{tiempo : 114187, tecla : 107, tecla2 : 115, tipo : 25},{tiempo : 116161, tecla : 107, tecla2 : 100, tipo : 50},{tiempo : 118496, tecla : 107, tecla2 : 100, tipo : 25},{tiempo : 121506, tecla : 106, tecla2 : 115, tipo : 25},{tiempo : 122744, tecla : 106, tecla2 : 115, tipo : 50},{tiempo : 124561, tecla : 106, tecla2 : 100, tipo : 25},{tiempo : 126337, tecla : 106, tecla2 : 100, tipo : 25},{tiempo : 128504, tecla : 106, tecla2 : 115, tipo : 50},{tiempo : 131635, tecla : 106, tecla2 : 100, tipo : 25},{tiempo : 132801, tecla : 107, tecla2 : 115, tipo : 25},{tiempo : 134735, tecla : 107, tecla2 : 100, tipo : 50},{tiempo : 136487, tecla : 106, tecla2 : 100, tipo : 50},{tiempo : 138985, tecla : 107, tecla2 : 115, tipo : 25},{tiempo : 140104, tecla : 107, tecla2 : 100, tipo : 50},{tiempo : 142820, tecla : 107, tecla2 : 115, tipo : 50},{tiempo : 144874, tecla : 106, tecla2 : 115, tipo : 25},{tiempo : 146377, tecla : 106, tecla2 : 115, tipo : 50},{tiempo : 149598, tecla : 106, tecla2 : 115, tipo : 25},{tiempo : 150705, tecla : 107, tecla2 : 115, tipo : 50},{tiempo : 153589, tecla : 106, tecla2 : 115, tipo : 25},{tiempo : 154424, tecla : 106, tecla2 : 115, tipo : 25},{tiempo : 157302, tecla : 106, tecla2 : 100, tipo : 25},{tiempo : 158215, tecla : 107, tecla2 : 100, tipo : 25},{tiempo : 160542, tecla : 106, tecla2 : 100, tipo : 25},{tiempo : 162456, tecla : 107, tecla2 : 100, tipo : 50},{tiempo : 165286, tecla : 106, tecla2 : 100, tipo : 25},{tiempo : 167001, tecla : 106, tecla2 : 115, tipo : 50},{tiempo : 168013, tecla : 106, tecla2 : 100, tipo : 50},{tiempo : 171625, tecla : 107, tecla2 : 115, tipo : 25},{tiempo : 173646, tecla : 106, tecla2 : 115, tipo : 25},{tiempo : 174754, tecla : 107, tecla2 : 100, tipo : 25},{tiempo : 176048, tecla : 107, tecla2 : 100, tipo : 50},{tiempo : 179871, tecla : 107, tecla2 : 100, tipo : 50},{tiempo : 180200, tecla : 107, tecla2 : 115, tipo : 25},{tiempo : 183893, tecla : 107, tecla2 : 100, tipo : 25},{tiempo : 184200, tecla : 107, tecla2 : 100, tipo : 25},{tiempo : 187933, tecla : 107, tecla2 : 115, tipo : 50},{tiempo : 189256, tecla : 106, tecla2 : 100, tipo : 25},{tiempo : 190053, tecla : 106, tecla2 : 115, tipo : 50},{tiempo : 193169, tecla : 107, tecla2 : 115, tipo : 25},{tiempo : 194366, tecla : 106, tecla2 : 100, tipo : 50},{tiempo : 196555, tecla : 107, tecla2 : 100, tipo : 50},{tiempo : 199554, tecla : 106, tecla2 : 100, tipo : 50}];
+var tiempos;
+var particles = [];
+var puntos = 0, puntos2 = 0;
+var startTime;
+var actual = 0;
+var playing;
+var settings;
+var multiplayer = false;
+var contadorBien = 0;
+var contadorMal = 0;
+var racha = 0;
+var canvas;
+var keys = [];
+
+window.onload = function () {
+  document.onkeydown = this.clic;
+  document.onkeyup = this.keysReleased;
+
+  iniciarPuntosyRacha();
+  drawInitialCanvas();
+
+  settings = {
+    density: 1,
+    startingX: canvas.width + 25
+    //startingY: 0
+  };
+
+  var tiempo = document.getElementById("tiempos").innerHTML;
+  tiempos = JSON.parse(tiempo);
+  cargarJuego();
+
+};
+
+//  Places a background image in a background canvas and draws the square over it
+function drawInitialCanvas() {
+  var canvasBg = document.getElementById("bg_canvas_1");
+  var contextBg = canvasBg.getContext("2d");
+  canvasBg.width = window.innerWidth;
+  canvasBg.height = 300;
+  var bgImg = new Image();
+  
+  bgImg.src = "../img/image.png";
+  bgImg.onload = function () {
+    //contextBg.drawImage(bgImg, 0, 0);
+    drawPattern(contextBg, canvasBg, bgImg);
+  }
+
+  //  Sets the width and height of the canvas in which the circles are going to be drawn
+  canvas = document.getElementById("canvas-1");
+  canvas.width = window.innerWidth;
+  canvas.height = 300;
+
+  if (multiplayer) {
+    var canvasBg2 = document.getElementById("bg_canvas_2");
+    var contextBg2 = canvasBg2.getContext("2d");
+    canvasBg2.width = window.innerWidth;
+    canvasBg2.height = 300;
+    var bgImg2 = new Image();
+
+    bgImg2.src = "../img/image.png";
+    bgImg2.onload = function () {
+      drawPattern(contextBg2, canvasBg2, bgImg2);
+    }
+
+    canvas = document.getElementById("canvas-2");
+    canvas.width = window.innerWidth;
+    canvas.height = 300;
+  }
+
+}
+
+
+function drawPattern(context, canvas, bgImg) {
+  //  Creates pattern to fill the entire canvas with the image
+  context.fillStyle = context.createPattern(bgImg, "repeat-x");
+  context.fillRect(0, 0, canvas.width, canvas.height);
+
+  //  Creates the square
+  /*var gradient = context.createLinearGradient(10, 50, 50, 10);
+  gradient.addColorStop("0", "magenta");
+  gradient.addColorStop("0.5", "blue");
+  gradient.addColorStop("1.0", "red");
+  context.strokeStyle = gradient;
+  context.lineWidth = 5;
+  context.strokeRect(30, 100, 100, 100);
+  */
+  var plato = new Image();
+  plato.src = '../img/plato.png';
+  plato.onload = function(){
+    context.drawImage(plato,30,60);
+  }
+  var gifCanvas = document.getElementById("gifCanvas");
+  gifler('../img/gatocome2.gif').animate(gifCanvas);
+}
+
+
+function cargarJuego() {
+  $("#player").bind("ended", function () {
+    var x = document.getElementById("myAudio");
+    //Aqui vamos a poner el audio que necesitamos
+    //x.src = '../media/' + 
+    x.play();
+    comprobar();
+
+    var canvas2, context2;
+    var canvas = document.getElementById("canvas-1");
+    var context = canvas.getContext("2d");
+    var rCanvas = document.createElement('canvas');
+    var rCtx = rCanvas.getContext('2d');
+    rCanvas.width = canvas.width;
+    rCanvas.height = canvas.height;
+    //Canvas azul
+    var aCanvas = document.createElement('canvas');
+    var aCtx = aCanvas.getContext('2d');
+    aCanvas.width = canvas.width;
+    aCanvas.height = canvas.height;
+    
+
+    loadDorayakis(canvas, context);
+    
+
+    if (multiplayer) {
+      canvas2 = document.getElementById("canvas-2");
+      context2 = canvas2.getContext("2d");
+    }
+    
+    for (var i = 0; i < tiempos.length; i++) {
+      particles.push({
+        x: settings.startingX,
+        //y: settings.startingY,
+        timing: tiempos[i].tiempo,
+        size: tiempos[i].tipo,
+        vx: 5,
+        moving: true,
+        tecla: tiempos[i].tecla,
+        tecla2: tiempos[i].tecla2,
+      });
+    }
+    playing = particles[0];
+    
+
+
+    window.requestAnimationFrame(animate);
+
+    function animate(time) {
+      // set startTime if it isn't already set
+      if (!startTime) {
+        startTime = time;
+      }
+      // calc elapsedTime
+      var elapsedTime = time - startTime;
+
+      context.clearRect(0, 0, canvas.width, canvas.height);
+      
+      if (multiplayer)
+        context2.clearRect(0, 0, canvas2.width, canvas2.height);
+
+      // assume no further animating is necessary
+      // The for-loop may change the assumption 
+      var continueAnimating = false;
+      for (var i = 0; i < particles.length; i++) {
+        var part = particles[i];
+
+        // update this circle & report if it wasMoved
+        var wasMoved = update(part, elapsedTime);
+        // if it wasMoved, then change assumption to continueAnimating
+        if (wasMoved || part.moving) {
+          continueAnimating = true;
+        }
+        // draw this arc at its current position
+        drawParticle(part);
+      }
+
+      // if update() reported that it moved something
+      // then request another animation loop
+      if (continueAnimating)
+        window.requestAnimationFrame(animate);
+    }
+
+    function update(part, elapsedTime) {
+      
+      if (elapsedTime >= part.timing) {
+        // is this arc still visible on the canvas
+        if (part.x > -part.size) {
+          // if yes+yes, move this arc by the specified moveX
+          part.x -= part.vx;
+          if (part.x <= 0) {
+            part.moving = false;
+          }
+          // report that we moved this arc
+          return (true);
+        }
+      }
+      
+      return (false);
+    }
+
+    function loadDorayakis(canvas, context){
+          // a ghost canvas that will keep our original image
+      //Canvas rojo
+
+     
+      //img c es por chiquito r es por rojo, g es por grande, a es por azul
+      var imgcr = new Image();
+      var imgca = new Image();
+
+      imgcr.onload = function() {
+        context.drawImage(imgcr, 0, 0, canvas.width, canvas.height);
+        rCtx.drawImage(canvas, 0, 0, canvas.width, canvas.height);
+        requestAnimationFrame(animate);
+      }
+      imgcr.src = "../img/RojoChiquito2.png";
+  
+      imgca.onload = function() {
+        context.drawImage(imgca, 0, 0, canvas.width, canvas.height);
+        aCtx.drawImage(canvas, 0, 0, canvas.width, canvas.height);
+        requestAnimationFrame(animate);
+      }
+      imgca.src = "../img/AzulEscalado.png";
+    
+    }
+    function drawParticle(part) {
+      //74 y 70 son sushis, 75 68 son dorayakis
+      context.beginPath();
+      var tam = [0,0];
+      var y = 0;
+      //Si el primer elemento de tecla es 100 o 102 se dibijan las cosas en pequeño
+      if(part.tecla[0] === 68 || part.tecla[0] === 70){
+        tam = [100,100];
+        y = 100;
+      }
+      //Si el primer elemento de tecla es 74 o 75 se dibujan en grande
+      else{
+        if(part.tecla[0] === 74)
+          tam = [170,170];  
+          y = 60;
+      }
+      //En este caso se dibujan sushis
+      if (part.tecla[0] === 75 || part.tecla[0] === 68){
+        context.drawImage(rCanvas, part.x, y, tam[0], tam[1]);
+              
+      }
+      //En este caso se dibujan dorayakis
+      else if (part.tecla[0] === 70 || part.tecla[0] === 74){
+        context.drawImage(aCanvas, part.x, y, tam[0], tam[1]);
+      }
+      //context.arc(part.x, part.y, part.size, 0, Math.PI * 2);
+      //context.fill();
+
+      if (multiplayer) {
+        context2.beginPath();
+        if (part.tecla2 === 107)
+          context2.fillStyle = "cyan";
+        else if (part.tecla2 === 106)
+          context2.fillStyle = "red";
+        context2.arc(part.x, part.y, part.size, 0, Math.PI * 2);
+        context2.fill();
+      }
+    }
+
+  });
+
+}
+
+
+function clic (evt) { 
+  //Se supone que se dibujan las cosas en pequeño si son las teclas 100 y 102 la primera tecla
+  keys[evt.keyCode] = true;
+  //Si se requiere pequeño
+  if(playing.tecla[0] === 68 || playing.tecla[0] === 70){
+    //Si se pulsa alguna de las teclas y es pequeño, acertamos
+    if (keys[playing.tecla[0]] || keys[playing.tecla[1]]){
+      compruebaAcierto(evt); 
+      ponAFalse(keys);
+    }
+  }
+  //Si se requiere grande
+  else if(playing.tecla[0] === 74 || playing.tecla[0] === 75){
+    //Si se pulsan las dos teclas requeridas para grande y es grande acertamos
+    if (keys[playing.tecla[0]] && keys[playing.tecla[1]]){
+      compruebaAcierto(evt);
+      ponAFalse(keys);
+    }
+  }
+  
+}
+
+/*function keysReleased(e) {
+	// mark keys that were released
+	keys[e.keyCode] = false;
+}*/
+function ponAFalse(keys){
+  keys[68] = false;
+  keys[70] = false;
+  keys[74] = false;
+  keys[75] = false;
+}
+function compruebaAcierto(e){
+  if ((playing.x >= 20 && playing.x <= 69) || (playing.x >= 91 && playing.x <= 140)) {
+    contadorBien++;
+    res = rachas(50, contadorBien);
+    racha = res[0];
+    puntos += res[1];
+  } else if (playing.x >= 70 && playing.x <= 90) {
+    contadorBien++;
+    res = rachas(100, contadorBien);
+    racha = res[0];
+    puntos += res[1];
+  } else {
+    contadorMal++;
+    contadorBien = 0;
+    racha = 1;
+    if (contadorMal >= 5)
+      puntos -= 25;
+  }
+
+  document.getElementById("puntos-p1").innerHTML = "Puntos: " + puntos;
+  document.getElementById("racha-p1").innerHTML = "Racha: " + racha;
+
+  //if (multiplayer)
+    //document.getElementById("puntos-p1").innerHTML = "Puntos jugador 1: " + puntos;
+}
+//Devuelve el computo de puntos y la racha
+function rachas(puntos, contadorBien){
+  if(contadorBien>= 2 && contadorBien < 6)
+    return [2,puntos * 2];
+  else if(contadorBien >= 6 && contadorBien < 10)
+    return [3,puntos * 3];
+  else if(contadorBien>=10 && contadorBien < 15)
+    return [4,puntos * 4];
+  else if(contadorBien>= 15 && contadorBien< 20)
+    return [5,puntos * 5];
+  else if(contadorBien>= 20)
+    return [10,puntos * 10];
+  else return [1,puntos];
+}
+/*function clic(evObject) {
+  var tecla = evObject.keyCode;
+  console.log(tecla, playing.tecla[0]);
+  if (tecla === playing.tecla[0] || tecla === playing.tecla[1]) {
+    //De 135 a 165 es pleno
+    
+  }
+
+  if (multiplayer) {
+    if (tecla === playing.tecla2) {
+      //De 135 a 165 es pleno
+      if ((playing.x >= 96 && playing.x <= 139) || (playing.x >= 161 && playing.x <= 204)) {
+        contadorBien++;
+        if (contadorBien >= 10)
+          puntos += 50 * 2;
+        else puntos += 50;
+      } else if (playing.x >= 140 && playing.x <= 160) {
+        contadorBien++;
+        if (contadorBien >= 10)
+          puntos += 100 * 2.5;
+        else puntos += 100;
+
+      } else {
+        contadorMal++;
+        contadorBien = 0;
+        if (contadorMal >= 5)
+          puntos -= 25;
+      }
+
+      document.getElementById("puntos-p2").innerHTML = "Puntos jugador 2: " + puntos2;
+    }
+  }
+}*/
+
+
+function iniciarPuntosyRacha() {
+  document.getElementById("puntos-p1").innerHTML = "Puntos: " + puntos;
+  document.getElementById("racha-p1").innerHTML = "Racha: " + racha;
+  if (multiplayer) {
+    document.getElementById("puntos-p1").innerHTML = "Puntos jugador 1: " + puntos;
+    var p2 = document.getElementById("puntos-p2");
+    p2.style.visibility = "visible";
+    p2.innerHTML = "Puntos jugador 2: " + puntos2;
+  }
+}
+
+
+function comprobar() {
+  var id = setInterval(comprueba, 1);
+}
+
+function comprueba() {
+  if (playing !== -1 && playing.x <= 0) {
+    actual += 1;
+    if (actual < particles.length) {
+      playing = particles[actual];
+    } else playing = -1;
+  }
+}
+
+
