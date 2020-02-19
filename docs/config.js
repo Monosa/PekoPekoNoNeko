6524d2c83ecb5ec2d30a336b63d4cb4de7298690
lelex22 <alpedros@ucm.es>
2020-02-19 12:54:08 +0100

    Editado config

--- docs/config.js
+++ docs/config.js
@@ -1,8 +1,11 @@
 const MongoClient = require('mongodb').MongoClient;
 const uri = "mongodb+srv://admin:adminpeko@cluster0-mqicg.mongodb.net/test?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
+MongoClient.connect(url,{ useUnifiedTopology: true, useNewUrlParser: true },function(err){
+    if(err){
+        console.log('Error in connection');
+    }
+    else{
+        console.log('Connected!');
+    }
 });
-module.exports=client;
+module.exports=MongoClient;
